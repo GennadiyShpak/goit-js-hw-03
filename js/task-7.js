eef7fdf4ccf34e5ae9d279142b8374e39e0dd3cb
Gennadiy <borchuga@gmail.com>
2020-08-14 17:54:48 +0300

    dev version

--- js/task-7.js
+++ js/task-7.js
@@ -1,90 +1,89 @@
-// Напиши скрипт управления личным кабинетом интернет банка. Есть объект account в котором необходимо реализовать методы для работы с балансом
-// и историей транзакций.
-
-/*
- * Типов транзацкий всего два.
- * Можно положить либо снять деньги со счета.
- */
 const Transaction = {
   DEPOSIT: 'deposit',
   WITHDRAW: 'withdraw',
 };
 
-/*
- * Каждая транзакция это объект со свойствами: id, type и amount
- */
-
 const account = {
   balance: 0,
 
-  // История транзакций
   transactions: [],
 
-  /*
-   * Метод создает и возвращает объект транзакции.
-   * Принимает сумму и тип транзакции.
-   */
   createTransaction(amount, type) {
-    const ids = function () {
-      let a = Math.trunc(Math.random() * 1e8);
-      return a;
+    const idTransaction = () => {
+      return this.transactions.length + 1;
     };
-    this.transactions['id'] = ids();
-    this.transactions['amount'] = amount;
-    if (amount > 0) {
-      this.transactions['type'] = 'deposit';
-    }
-    if (amount < 0) {
-      this.transactions['type'] = 'withdraw';
-    }
-
-    return this.transactions;
+    return {
+      id: idTransaction(),
+      amount: amount,
+      type: type,
+    };
   },
 
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
   deposit(amount) {
-    const { DEPOSIT, WITHDRAW } = Transaction;
-    console.log(DEPOSIT);
-    console.log(WITHDRAW);
-    // this.createTransaction();
+    const { DEPOSIT } = Transaction;
+    let result;
+    if (amount === 0 || amount < 0) {
+      result =
+        'Введено не коректное значение, пожалуйста введите сумму больше 0';
+    } else {
+      this.transactions.push(this.createTransaction(amount, DEPOSIT));
+      this.balance += amount;
+      result = `На счет добавленно ${amount} у.е.`;
+    }
+    return result;
   },
 
-  /*
-   * Метод отвечающий за снятие суммы с баланса.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций.
-   *
-   * Если amount больше чем текущий баланс, выводи сообщение
-   * о том, что снятие такой суммы не возможно, недостаточно средств.
-   */
-  //   withdraw(amount) {},
+  withdraw(amount) {
+    const { WITHDRAW } = Transaction;
+    let result;
+
+    if (amount === 0 || amount < 0) {
+      result =
+        'Введено не коректное значение, пожалуйста введите сумму больше 0';
+    }
+    if (amount > this.balance) {
+      result = 'На счету недостаточно средств, пожалуйста введите другую сумму';
+    } else {
+      this.transactions.push(this.createTransaction(amount, WITHDRAW));
+      this.balance -= amount;
+      result = `С вашего счета снято ${amount} у.е.`;
+    }
+    return result;
+  },
 
   /*
    * Метод возвращает текущий баланс
    */
-  //   getBalance() {},
+  getBalance() {
+    return this.balance;
+  },
 
-  /*
-   * Метод ищет и возвращает объект транзации по id
-   */
-  //   getTransactionDetails(id) {},
+  getTransactionDetails(id) {
+    this.transactions.forEach(transaction => {
+      if (id == transaction.id) return console.log(transaction);
+    });
+  },
 
-  /*
-   * Метод возвращает количество средств
-   * определенного типа транзакции из всей истории транзакций
-   */
-  //   getTransactionTotal(type) {},
+  getTransactionTotal(type) {
+    let result = 0;
+    this.transactions.forEach(a => {
+      if (type === a.type) {
+        result += a.amount;
+      }
+      return console.log(result);
+    });
+  },
 };
-
-// console.table(account.createTransaction(-550));
-// account.deposit({ amount: 550, type: 'deposit' });
-console.table(account.deposit(550));
-// console.log(account.createTransaction(550, 'deposit'));
-// console.log(account.createTransaction(22, 'deposit'));
-// console.log(account.createTransaction(55540, 'deposit'));
+console.table(account.transactions);
+console.table(account.deposit(10));
+console.table(account.deposit(0));
+console.table(account.deposit(25));
+console.table(account.deposit(30));
+console.table(account.deposit(40));
+console.table(account.withdraw(10));
+console.table(account.withdraw(20));
+console.table(account.getTransactionDetails(2));
+// console.table(account.getBalance());
+// console.table(account.getTransactionTotal('deposit'));
+// console.table(account.getTransactionTotal('withdraw'));
+// console.table(account.transactions.type);
